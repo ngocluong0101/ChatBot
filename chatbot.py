a2d1d804374f1c3aa0cda463ebb03fcf8e1eaada
NgocLuong <ngocluongg0101@gmail.com>
2026-08-17 21:24:46 +0700

    update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,16 @@
 from langchain_community.document_loaders import DirectoryLoader, UnstructuredFileLoader
-
+from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.utils import DistanceStrategy
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 loaders = DirectoryLoader(
     path = "./papers",
     glob = "**/*.pdf",
@@ -32,6 +42,50 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 splits = text_splitter.split_documents(docs)
-from pprint import pprint
 
-pprint(splits) 
+embeddings = OpenAIEmbeddings(
+    model = "text-embedding-3-small",
+)
+
+vectorstore = FAISS.from_documents(
+    documents = splits,
+    embedding = embeddings,
+    distance_strategy = DistanceStrategy.COSINE
+)
+
+retriever = vectorstore.as_retriever(
+    search_type = "similarity_score_threshold",
+    search_kwargs = {"k": 5, "score_threshold": 0.2}
+)
+
+template = (
+    "You are a strict, citation-focused assistant for a private knowledge base.\n"
+    "RULES:\n"
+    "1) Use ONLY the provided context to answer.\n"
+    "2) If the answer is not clearly contained in the context, say: "
+    "\"I don't know based on the provided documents.\"\n"
+    "3) Do NOT use outside knowledge, guessing, or web information.\n"
+    "4) If applicable, cite sources as (source:page) using the metadata.\n\n"
+    "Context:\n{context}\n\n"
+    "Question: {question}"
+)
+
+prompt = ChatPromptTemplate.from_template(template)
+
+llm = ChatOpenAI(
+    model = "gpt-5-mini",
+    temperature = 0
+)
+
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+question = input("Question: ")
+
+answer = rag_chain.invoke(question) 
+
+print(answer)
